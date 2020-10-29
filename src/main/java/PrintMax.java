8a0af4420af320e1818cbf491ca7a24a8b8a81fa
b115030 <iitiangokul@gmail.com>
2020-10-29 11:37:42 +0530

    ADDED: method and test cases to print max Float

--- src/main/java/PrintMax.java
+++ src/main/java/PrintMax.java
@@ -3,9 +3,20 @@ public class PrintMax {
         System.out.println("Welcome to print max");
         return true;
     }
+
     //Use Case 1
     public Integer printMaxInteger(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
         Integer maxNumber = firstNumber;
+        if (maxNumber.compareTo(secondNumber) < 0)
+            maxNumber = secondNumber;
+        if (maxNumber.compareTo(thirdNumber) < 0)
+            maxNumber = thirdNumber;
+        return maxNumber;
+    }
+
+    //Use case 2
+    public Float printMaxFloat(Float firstNumber, Float secondNumber, Float thirdNumber) {
+        Float maxNumber = firstNumber;
         if (maxNumber.compareTo(secondNumber) < 0)
             maxNumber = secondNumber;
         if (maxNumber.compareTo(thirdNumber) < 0)
--- src/test/java/PrintMaxTest.java
+++ src/test/java/PrintMaxTest.java
@@ -15,22 +15,46 @@ public class PrintMaxTest {
         boolean returnWeclome = printMax.printWelcome();
         Assert.assertTrue(returnWeclome);
     }
-    //Test Case 1.1
+
+    //Test Case 1.1 prints max Integer when max Integer is at first position
     @Test
     public void whenMaxIntegerIsFirstArgument_ShouldReturnFirstArgument() {
         Integer maxNumber = printMax.printMaxInteger(86, 85, 45);
         Assert.assertEquals((Integer) 86, maxNumber);
     }
-    //test case 1.2
+
+    //test case 1.2 prints max Integer when max Integer is at second position
     @Test
     public void whenMaxIntegerIsSecondArgument_ShouldReturnSecondArgument() {
         Integer maxNumber = printMax.printMaxInteger(25, 268, 15);
         Assert.assertEquals((Integer) 268, maxNumber);
     }
-    //test case 1.3
+
+    //test case 1.3 prints max Integer when max Integer is at third position
     @Test
     public void whenMaxIntegerIsThirdArgument_ShouldReturnThirdArgument() {
         Integer maxNumber = printMax.printMaxInteger(25, 26, 150);
         Assert.assertEquals((Integer) 150, maxNumber);
+    }
+
+    //test case 2.1 prints max Float when max Float is at first position
+    @Test
+    public void whenMaxFloatIsFirstArgument_ShouldReturnFirstArgument() {
+        Float maxNumber = printMax.printMaxFloat(26.1256f, 26.05268f, 26.124896f);
+        Assert.assertEquals((Float) 26.1256f, maxNumber);
+    }
+
+    //test case 2.2 prints max Float when max Float is at second position
+    @Test
+    public void whenMaxFloatIsSecondArgument_ShouldReturnSecondArgument() {
+        Float maxNumber = printMax.printMaxFloat(26.1256f, 26.35268f, 26.324896f);
+        Assert.assertEquals((Float) 26.35268f, maxNumber);
+    }
+
+    //test case 2.3 prints max Float when max Float is at third position
+    @Test
+    public void whenMaxFloatIsThirdArgument_ShouldReturnThirdArgument() {
+        Float maxNumber = printMax.printMaxFloat(26.1256f, 26.35268f, 26.374896f);
+        Assert.assertEquals((Float) 26.374896f, maxNumber);
     }
 }
