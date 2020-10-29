7089c85f126a2d05f90406139da73f70ce11b19e
b115030 <iitiangokul@gmail.com>
2020-10-29 13:04:21 +0530

    REFACTORED: generics class to pass 3 generic variables and print max

--- src/main/java/PrintMax.java
+++ src/main/java/PrintMax.java
@@ -1,14 +1,27 @@
-public class PrintMax {
-    public boolean printWelcome() {
+public class PrintMax<E extends Comparable> {
+    E firstObject, secondObject, thirdObject;
+
+    public PrintMax(E firstObject, E secondObject, E thirdObject) {
+        this.firstObject = firstObject;
+        this.secondObject = secondObject;
+        this.thirdObject = thirdObject;
+    }
+
+    public static boolean printWelcome() {
         System.out.println("Welcome to print max");
         return true;
     }
-    public <E extends Comparable> E printMaxElement(E firstElement, E secondElement, E thirdElement){
-        E maxElement= firstElement;
-        if (maxElement.compareTo(secondElement)<0)
-            maxElement=secondElement;
-        if (maxElement.compareTo(thirdElement)<0)
-            maxElement=thirdElement;
+
+    public static <E extends Comparable> E testMaximum(E firstElement, E secondElement, E thirdElement) {
+        E maxElement = firstElement;
+        if (maxElement.compareTo(secondElement) < 0)
+            maxElement = secondElement;
+        if (maxElement.compareTo(thirdElement) < 0)
+            maxElement = thirdElement;
         return maxElement;
+    }
+
+    public E testMaximum() {
+        return testMaximum(this.firstObject, this.secondObject, this.thirdObject);
     }
 }
--- src/test/java/PrintMaxTest.java
+++ src/test/java/PrintMaxTest.java
@@ -1,81 +1,74 @@
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class PrintMaxTest {
-    PrintMax printMax;
-
-    @Before
-    public void setUp() {
-        printMax = new PrintMax();
-    }
 
     @Test
     public void whenCallprintWelcomeMethod_shouldReturnTrue() {
-        boolean returnWeclome = printMax.printWelcome();
-        Assert.assertTrue(returnWeclome);
+        boolean returnWelcome = PrintMax.printWelcome();
+        Assert.assertTrue(returnWelcome);
     }
 
     //Test Case 1.1 prints max Integer when max Integer is at first position
     @Test
     public void whenMaxIntegerIsFirstArgument_ShouldReturnFirstArgument() {
-        Integer maxNumber = printMax.printMaxElement(86, 85, 45);
-        Assert.assertEquals((Integer) 86, maxNumber);
+        PrintMax maxElement = new PrintMax(86, 85, 45);
+        Assert.assertEquals(86, maxElement.testMaximum());
     }
 
     //test case 1.2 prints max Integer when max Integer is at second position
     @Test
     public void whenMaxIntegerIsSecondArgument_ShouldReturnSecondArgument() {
-        Integer maxNumber = printMax.printMaxElement(25, 268, 15);
-        Assert.assertEquals((Integer) 268, maxNumber);
+        PrintMax maxElement = new PrintMax(25, 268, 15);
+        Assert.assertEquals(268, maxElement.testMaximum());
     }
 
     //test case 1.3 prints max Integer when max Integer is at third position
     @Test
     public void whenMaxIntegerIsThirdArgument_ShouldReturnThirdArgument() {
-        Integer maxNumber = printMax.printMaxElement(25, 26, 150);
-        Assert.assertEquals((Integer) 150, maxNumber);
+        PrintMax maxElement = new PrintMax(25, 26, 150);
+        Assert.assertEquals(150, maxElement.testMaximum());
     }
 
     //test case 2.1 prints max Float when max Float is at first position
     @Test
     public void whenMaxFloatIsFirstArgument_ShouldReturnFirstArgument() {
-        Float maxNumber = printMax.printMaxElement(26.1256f, 26.05268f, 26.124896f);
-        Assert.assertEquals((Float) 26.1256f, maxNumber);
+        PrintMax maxElement = new PrintMax(26.1256f, 26.05268f, 26.124896f);
+        Assert.assertEquals(26.1256f, maxElement.testMaximum());
     }
 
     //test case 2.2 prints max Float when max Float is at Second position
     @Test
     public void whenMaxFloatIsSecondArgument_ShouldReturnSecondArgument() {
-        Float maxNumber = printMax.printMaxElement(26.1256f, 26.05268f, 26.124896f);
-        Assert.assertEquals((Float) 26.1256f, maxNumber);
+        PrintMax maxElement = new PrintMax(26.1256f, 26.05268f, 26.124896f);
+        Assert.assertEquals(26.1256f, maxElement.testMaximum());
     }
 
     //test case 2.3 prints max Float when max Float is at third position
     @Test
     public void whenMaxFloatIsThirdArgument_ShouldReturnThirdArgument() {
-        Float maxNumber = printMax.printMaxElement(26.1256f, 26.35268f, 26.374896f);
-        Assert.assertEquals((Float) 26.374896f, maxNumber);
+        PrintMax maxElement = new PrintMax(26.1256f, 26.35268f, 26.374896f);
+        Assert.assertEquals(26.374896f, maxElement.testMaximum());
     }
 
     //test case 3.1 prints max String when max String is at first position
     @Test
     public void whenMaxStringIsFirstArgument_ShouldReturnFirstArgument() {
-        String maxString = printMax.printMaxElement("yddhdhdassa", "gudbdbdn", "assas");
-        Assert.assertEquals((String) "yddhdhdassa", maxString);
+        PrintMax maxElement = new PrintMax("yddhdhdassa", "gudbdbdn", "assas");
+        Assert.assertEquals("yddhdhdassa", maxElement.testMaximum());
     }
 
     //test case 3.2 prints max String when max String is at Second position
     @Test
     public void whenMaxStringIsSecondArgument_ShouldReturnSecondArgument() {
-        String maxString = printMax.printMaxElement("aodlsdj", "bdhjdjhj", "ahhdhd");
-        Assert.assertEquals((String) "bdhjdjhj", maxString);
+        PrintMax maxElement = new PrintMax("aodlsdj", "bdhjdjhj", "ahhdhd");
+        Assert.assertEquals("bdhjdjhj", maxElement.testMaximum());
     }
 
     //test case 3.3 prints max String when max String is at third position
     @Test
     public void whenMaxStringIsThirdArgument_ShouldReturnThirdArgument() {
-        String maxString = printMax.printMaxElement("ahghddhdshj", "sjdskjkddd", "tewed");
-        Assert.assertEquals((String) "tewed", maxString);
+        PrintMax maxElement = new PrintMax("ahghddhdshj", "sjdskjkddd", "tewed");
+        Assert.assertEquals("tewed", maxElement.testMaximum());
     }
 }
